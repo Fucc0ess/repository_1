495cc88be57d60b4a84eeb684e5ed13772107d83
Fucc0ess <s0169526@edu.kubsu.ru>
2023-10-13 23:23:38 +0300

    Изменил code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
-include <iostream>
-int main(){
-   std::string name;
-   std::cout<<"Введите ваше имя"; //Enter username
-   std::cin>>name;
-   std::cout<<"Hello world "<<name<<endl; //Output Hello world with the username
-   return 0;
+include<iostream>
+int
+main()
+{
+  std::string name;
+  std::cout << "Введите ваше имя"; //Enter username
+  std::cin >> name;
+  std::cout << "Hello world " << name
+            << endl; //Output Hello world with the username
+  return 0;
 }
